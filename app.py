4e14f798c68872407f194552f6577dba50b6fa76
iainm342 <ism342@gmail.com>
2021-02-08 09:23:55 +0000

    added login @app in app.py

--- app.py
+++ app.py
@@ -46,6 +46,28 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one({"username": request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for("profile", username=session["user"]))
+            else:
+                flash("incorrect username and/or password")
+                return redirect(url_for("login"))
+        
+        else:
+            flash("incorrect username and/or password")
+            return redirect(url_for("login"))
+    
+    return render_template("login.html")
+
+
+
 @app.route("/profile/<username>", methods={"GET", "POST"})
 def profile(username):
     username = db.mongo.db.users.find_one(
