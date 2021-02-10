741b190f4defa978a68966bc8dc17d5812b3d175
iainm342 <ism342@gmail.com>
2021-02-10 20:56:59 +0000

    added jQuery for dynamically add/remove input

--- static/js/recipe.js
+++ static/js/recipe.js
@@ -1,18 +1,35 @@
 $(document).ready(function() {
-	var max_fields      = 10; //maximum input boxes allowed
-	var wrapper   		= $(".input_fields_wrap"); //Fields wrapper
-	var add_button      = $(".add_field_button"); //Add button ID
+	var max_ing      		= 40; //maximum input boxes allowed
+	var wrapper_ing   		= $(".input_ing_wrap"); //Fields wrapper
+	var add_ing_button      = $(".add_ing_button"); //Add button ID
 	
-	var x = 1; //initlal text box count
-	$(add_button).click(function(e){ //on add input button click
+	var ing = 1; //initlal text box count
+	$(add_ing_button).click(function(e){ //on add input button click
 		e.preventDefault();
-		if(x < max_fields){ //max input box allowed
-			x++; //text box increment
-			$(wrapper).append('<div><input id="recipe_ingredients" type="text" name="recipe_ingredients"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+		if(ing < max_ing) { //max input box allowed
+			ing++; //text box increment
+			$(wrapper_ing).append('<div><input id="recipe_ingredients" type="text" name="recipe_ingredients"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
 		}
 	});
 	
-	$(wrapper).on("click",".remove_field", function(e){ //user click on remove text
-		e.preventDefault(); $(this).parent('div').remove(); x--;
+	$(wrapper_ing).on("click",".remove_field", function(e){ //user click on remove text
+		e.preventDefault(); $(this).parent('div').remove(); ing--;
 	});
-})
+
+	var max_meth      		= 40; //maximum input boxes allowed
+	var wrapper_meth   		= $(".input_meth_wrap"); //Fields wrapper
+	var add_meth_button      = $(".add_meth_button"); //Add button ID
+	
+	var meth = 1; //initlal text box count
+	$(add_meth_button).click(function(e){ //on add input button click
+		e.preventDefault();
+		if(ing < max_meth) { //max input box allowed
+			ing++; //text box increment
+			$(wrapper_meth).append('<div><input id="recipe_method" type="text" name="recipe_method"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+		}
+	});
+	
+	$(wrapper_meth).on("click",".remove_field", function(e){ //user click on remove text
+		e.preventDefault(); $(this).parent('div').remove(); ing--;
+	});
+});
