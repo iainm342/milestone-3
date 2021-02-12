c9d67867aae5a2c7e6a631cb97c5aa01e91c7e21
iainm342 <ism342@gmail.com>
2021-02-12 15:18:08 +0000

    changed col layout in add/edit pages

--- static/js/recipe.js
+++ static/js/recipe.js
@@ -1,35 +1,35 @@
 $(document).ready(function() {
-	var max_ing      		= 40; //maximum input boxes allowed
-	var wrapper_ing   		= $(".input_ing_wrap"); //Fields wrapper
-	var add_ing_button      = $(".add_ing_button"); //Add button ID
+	var max_ing      		= 40;
+	var wrapper_ing   		= $(".input_ing_wrap");
+	var add_ing_button      = $(".add_ing_button");
 	
-	var ing = 1; //initlal text box count
-	$(add_ing_button).click(function(e){ //on add input button click
+	var ing = 1;
+	$(add_ing_button).click(function(e){
 		e.preventDefault();
-		if(ing < max_ing) { //max input box allowed
-			ing++; //text box increment
-			$(wrapper_ing).append('<div><input id="recipe_ingredients" type="text" name="recipe_ingredients"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+		if(ing < max_ing) { 
+			ing++; 
+			$(wrapper_ing).append('<div class="col s10"><input id="recipe_ingredients" type="text" name="recipe_ingredients"/><a href="#" class="remove_field">Remove</a></div>');
 		}
 	});
 	
-	$(wrapper_ing).on("click",".remove_field", function(e){ //user click on remove text
+	$(wrapper_ing).on("click",".remove_field", function(e){ 
 		e.preventDefault(); $(this).parent('div').remove(); ing--;
 	});
 
-	var max_meth      		= 40; //maximum input boxes allowed
-	var wrapper_meth   		= $(".input_meth_wrap"); //Fields wrapper
-	var add_meth_button      = $(".add_meth_button"); //Add button ID
+	var max_meth      		= 40; 
+	var wrapper_meth   		= $(".input_meth_wrap"); 
+	var add_meth_button      = $(".add_meth_button");
 	
-	var meth = 1; //initlal text box count
-	$(add_meth_button).click(function(e){ //on add input button click
+	var meth = 1; 
+	$(add_meth_button).click(function(e){ 
 		e.preventDefault();
-		if(ing < max_meth) { //max input box allowed
-			ing++; //text box increment
-			$(wrapper_meth).append('<div><input id="recipe_method" type="text" name="recipe_method"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+		if(ing < max_meth) {
+			ing++; 
+			$(wrapper_meth).append('<div><input id="recipe_method" type="text" name="recipe_method"/><button class=""<a href="#" class="remove_field">Remove</a></div>'); 
 		}
 	});
 	
-	$(wrapper_meth).on("click",".remove_field", function(e){ //user click on remove text
+	$(wrapper_meth).on("click",".remove_field", function(e){
 		e.preventDefault(); $(this).parent('div').remove(); ing--;
 	});
 });
