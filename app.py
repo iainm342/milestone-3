cdc8ec884765f8f0ef61df9340d66975344671a3
iainm342 <ism342@gmail.com>
2021-02-22 09:54:16 +0000

    amended edit_profile page and route after confirm

--- app.py
+++ app.py
@@ -152,11 +152,11 @@ def edit_profile(username):
             }
             mongo.db.users.update({"username": username.lower()}, update)
             flash("user info updated")
+            return redirect(url_for('profile', username=session['user']))
     
         if "user" in session:
             return render_template("edit_profile.html", user=user)
 
-        return redirect(url_for("login"))
 
 
 @app.route("/delete_profile/<username>")
