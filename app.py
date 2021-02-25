98d339b7dda6b46346c27d3c0848892327a61cd3
iainm342 <ism342@gmail.com>
2021-02-25 10:47:54 +0000

    added pop to remove password on profile screen

--- app.py
+++ app.py
@@ -112,7 +112,6 @@ def register():
 def login():
     if request.method == "POST":
         existing_user = mongo.db.users.find_one({"username": request.form.get("username").lower()})
-
         if existing_user:
             if check_password_hash(existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
@@ -132,10 +131,11 @@ def login():
 @app.route("/profile/<username>", methods={"GET", "POST"})
 def profile(username):
     user = mongo.db.users.find_one({"username": username.lower()})
-
+    print (user,session)
     if "user" in session:
+        user.pop("password")
         return render_template("profile.html", user=user)
-    
+
     return redirect(url_for("login"))
 
 
