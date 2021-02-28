74999f7d5571b8c6aac5e1ecfbeff34e098ea544
iainm342 <ism342@gmail.com>
2021-02-28 17:50:27 +0000

    added emailjs scripts and email to register.html

---
+++ static/js/sendEmail.js
@@ -0,0 +1,13 @@
+//New User Sign Up
+
+var emailjs;
+
+let form = document.getElementById("contact");
+
+//function to activate EmailJS API
+function sendMail(contactForm) {
+    emailjs.send("gmail", "registration", {
+        "from_name": contactForm.user_first.value,
+        "from_email": contactForm.user_email.value,
+    });
+}
