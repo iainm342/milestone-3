6747e6f2b41943f2e271944f8af0bb9d63104626
iainm342 <ism342@gmail.com>
2021-03-01 11:21:16 +0000

    deleted textarea js that wasn't needed

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,4 @@ $(document).ready(function(){
     $(".dropdown-trigger").dropdown();
     $('.sidenav').sidenav({edge: "right"});
     $('select').formSelect();
-    $('#recipe_method').val();
-    M.textareaAutoResize($('#recipe_method'));
 });
