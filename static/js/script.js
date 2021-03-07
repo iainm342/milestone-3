b92e794301d49aaf452ca7915e4905fe58b2da62
iainm342 <ism342@gmail.com>
2021-03-07 21:49:16 +0000

    added back to top button in base.html

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,23 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('select').formSelect();
 });
+
+// Sroll to top script
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+                mybutton.style.display = "none";
+        }
+    }
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
